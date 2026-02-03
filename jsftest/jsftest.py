68e6224aed6f9d1be2ed1424cc9aacae86a6ee61
wangjingyi32 <wangjingyi32@jd.com>
2026-02-03 15:21:20 +0800

    优化提示词

--- jsftest/jsftest.py
+++ jsftest/jsftest.py
@@ -21,10 +21,13 @@ async def main():
     task = (
         f"Step 1: Navigate exactly to this URL without any modification:\n{full_url}\n\n"
         "Step 2: 在这个配置页面点击新增\n"
-        "Step 3: 接口名输入com.jd.jsf.service.DemoService进行检索并选中，别名填入JSF-COPPER-SIDECAR进行检索并选中，方法填入sayHello进行检索并选中，其他配置用默认值\n"
-        "Step 4: 点击提交\n"
-        "Step 5: 点击查询\n"
-        "Step 6: 查询到的结果中，选择第一个，将操作的开关打开并提交开启\n"
+        "Step 3: 接口名输入com.jd.jsf.service.DemoService进行检索，等待数据加载完毕后,可以下拉下拉列表来查找选中,要求必须精确匹配\n"
+        "Step 4: 别名填入JSF-COPPER-SIDECAR进行检索,等待数据加载完毕后,可以下拉下拉列表来查找选中,要求必须精确匹配\n"
+        "Step 5:方法填入sayHello进行检索,等待数据加载完毕后,可以下拉下拉列表来查找选中,要求必须精确匹配\n"
+        "Step 6:其他配置用默认值\n"
+        "Step 7: 点击提交\n"
+        "Step 8: 点击查询\n"
+        "Step 9: 查询到的结果中，选择第一个，将操作的开关打开并提交开启\n"
 
     )
 
